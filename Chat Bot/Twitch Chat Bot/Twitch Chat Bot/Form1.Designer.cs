65317fbcf03e5f2eed7e35a71acd1b7968d60e0b
NO8STROMO <johntpaul93@gmail.com>
2017-04-03 14:57:06 -0400

    Outlined code from HD: "Twitch Chat Bot 101 C#."

---
+++ Chat Bot/Twitch Chat Bot/Twitch Chat Bot/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Chat_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Chat = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Chat
+            // 
+            this.Chat.AutoSize = true;
+            this.Chat.Location = new System.Drawing.Point(13, 13);
+            this.Chat.Name = "Chat";
+            this.Chat.Size = new System.Drawing.Size(35, 13);
+            this.Chat.TabIndex = 0;
+            this.Chat.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Chat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Chat;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Chat Bot/Twitch Chat Bot/Twitch Chat Bot/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Chat_Bot
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        StreamReader reader;
+        StreamWriter writer;
+
+        readonly string chatMessagePrefix;
+        readonly string userName;
+        readonly string password;
+        readonly string chatCommandID;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Reconnect();
+        }
+
+        private void Reconnect()
+        {
+            tcpClient = new TcpClient(); //Takes in IP adress and port 
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+
+            writer.WriteLine(""); //send username and password
+
+            writer.WriteLine(""); //select channel to join
+            writer.Flush();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!tcpClient.Connected)
+            {
+                Reconnect();
+            }
+
+            if (tcpClient.Available > 0 || reader.Peek() >= 0)
+            {
+                var message = reader.ReadLine();
+
+                var iPrefix = message.IndexOf(""); 
+                if(iPrefix > 0)
+                {
+                    var command = message.Substring(iPrefix + 1);
+                    if(message.Contains(chatCommandID))
+                    {
+                        var iBang = command.IndexOf("!");
+                        if(iBang > 0)
+                        {
+                            var speaker = command.Substring(0, iBang);
+                            var chatMessage = message.Substring(chatMessagePrefix.Length + 2);
+
+                            ReceiveMessage(speaker, chatMessage);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        void ReceiveMessage(string speaker, string message)
+        {
+            Chat.Text += $"\r\n {speaker} : {message}";
+        }
+
+        void SendMessage(string message)
+        {
+            writer.WriteLine(message);
+            writer.Flush();
+        }
+    }
+}
