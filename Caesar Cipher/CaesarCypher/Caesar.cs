39375c750dcea6284d28075ca875943cad0283fa
NO8STROMO <johntpaul93@gmail.com>
2017-06-26 03:43:15 -0400

    Changes to comment formatting.

--- Caesar Cipher/CaesarCypher/Caesar.cs
+++ Caesar Cipher/CaesarCypher/Caesar.cs
@@ -4,29 +4,29 @@ namespace CaesarCipher
 {
     class Caesar
     {
-        //Create an array, alphabet, to store every char in the alphabet 
+        // Create an array, alphabet, to store every char in the alphabet 
         static char[] alphabet = {'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         static void Main(string[] args)
         {
-            //Prompt the user to enter a string to be encoded do decoded 
+            // Prompt the user to enter a string to be encoded do decoded 
             Console.WriteLine("Please enter a string: ");
-            //Read and store that string userInput
+            // Read and store that string userInput
             string userInput = Console.ReadLine();
-            //Change userInput to all lower case letters
+            // Change userInput to all lower case letters
             string userInputLower = userInput.ToLower();
 
             int typeOfCipher = 0;
 
-            //Deals with user potentially entered an unreigested type of cipher
+            // Deals with user potentially entered an unreigested type of cipher
             while (typeOfCipher != 1 || typeOfCipher != 2)
             {
 
-                //Ask the user if they want to encrypt of decypt the string
+                // Ask the user if they want to encrypt of decypt the string
                 Console.WriteLine("Press 1 for Encypt and 2 for Decrypt: ");
 
-                //What if the user enters a letter or other character?
+                // What if the user enters a letter or other character?
                 try
                 {
                     typeOfCipher = Convert.ToInt32(Console.ReadLine());
@@ -36,7 +36,7 @@ namespace CaesarCipher
                     continue;
                 }
                 
-                //Encypting
+                // Encypting
                 if (typeOfCipher == 1)
                 {
                     //Generate a random number by which to shift the alphabet by
@@ -45,17 +45,18 @@ namespace CaesarCipher
 
                     string encrypted = "";
 
-                    //For each letter in the user input
+                    // For each letter in the user input
                     foreach (char letter in userInputLower)
                     {
-                        //Case to deal with spaces being entered
+                        // Case to deal with spaces being entered
                         if (letter == ' ')
                         {
                             encrypted += " ";
                         }
-                        //Shift each individual letter by the random shift and add that char to the encrypted string
+                        // Shift each individual letter by the random shift and add that char to the encrypted string
                         else
                         {
+                            // What does Array.IndexOf() do?
                             int curIndex = Array.IndexOf(alphabet, letter);
                             int shiftedIndex = (curIndex + shift) % alphabet.Length;
                             char encryptedChar = alphabet[shiftedIndex];
@@ -63,25 +64,25 @@ namespace CaesarCipher
                         }
                     }
 
-                    //Print out the encrypted 
+                    // Print out the encrypted 
                     Console.WriteLine(encrypted);
                     Console.ReadKey();
                     return;
                 }
 
 
-                //Decrypting
+                // Decrypting
                 else if (typeOfCipher == 2)
                 {
                     int isKeyProvided = 0;
                     string decrypted = " ";
 
-                    //Deals with user potentially entering a wrong answer to the prompt
+                    // Deals with user potentially entering a wrong answer to the prompt
                     while (isKeyProvided != 1 || isKeyProvided != 2)
                     {
-                        //Ask the user if the key is provided or not
+                        // Ask the user if the key is provided or not
                         Console.WriteLine("Is the Key Provided? Press 1 for YES and 2 for NO");
-                        ////What if the user enters a letter or other character?
+                        // What if the user enters a letter or other character?
                         try
                         {
                             isKeyProvided = Convert.ToInt32(Console.ReadLine());
@@ -91,7 +92,7 @@ namespace CaesarCipher
                             continue;
                         }
 
-                        //If the key is provided
+                        // If the key is provided
                         if (isKeyProvided == 1)
                         {
                             Console.WriteLine("What is the key: ");
@@ -100,13 +101,13 @@ namespace CaesarCipher
 
                             foreach (char letter in userInput)
                             {
-                                //Case to deal with spaces being entered
+                                // Case to deal with spaces being entered
                                 if (letter == ' ')
                                 {
                                     decrypted += " ";
                                 }
                                 else
-                                //Shift each individual character to the unshifted index and add it to decrypted
+                                // Shift each individual character to the unshifted index and add it to decrypted
                                 {
                                     int curIndex = Array.IndexOf(alphabet, letter);
                                     int unShiftedIndex = (curIndex - key) % 26;
@@ -116,37 +117,37 @@ namespace CaesarCipher
                                 }
                             }
 
-                            //Print out Decrypted 
+                            // Print out Decrypted 
                             Console.WriteLine(decrypted);
                             Console.ReadKey();
                             return;
                         }
 
-                        //If the key is not provided
+                        // If the key is not provided
                         else if (isKeyProvided == 2)
                         {
-                            //Check every potential decryption
+                            // Check every potential decryption
                             for (int i = 26; i >= 0; i--)
                             {
                                 decrypted = "";
                                 foreach (char letter in userInput)
 
                                 {
-                                    //Case to deal with spaces being entered
+                                    // Case to deal with spaces being entered
                                     if (letter == ' ')
                                     {
                                         decrypted += " ";
                                     }
                                     else
                                     {
-                                        //Shift each individual character to the unshifted index and add it to potential decrypted
+                                        // Shift each individual character to the unshifted index and add it to potential decrypted
                                         int curIndex = Array.IndexOf(alphabet, letter);
                                         int unShiftedIndex = (curIndex - i) % 26;
                                         char decryptedChar = alphabet[unShiftedIndex];
                                         decrypted += decryptedChar;
                                     }
                                 }
-                                //Display Potential cipher
+                                // Display Potential cipher
                                 Console.WriteLine(decrypted);   
                             }
                             Console.ReadKey();
