5c17467653e7eae3a0defcbc04e45a50b3e10d0b
John T Paul <johntpaul93@gmail.com>
2017-10-29 21:55:31 -0400

    Started Triangle Project

---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+  class Triangle
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter a width: ");
+      int width = Convert.ToInt32(Console.ReadLine());
+
+      for (int )
+    }
+  }
+}
